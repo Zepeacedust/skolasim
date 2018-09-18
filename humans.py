3e86ea96ad4d79fc85e942f7b84c1c3bc1673fee
Arnor <arnor0903@gmail.com>
2018-09-18 14:07:55 +0000

    byrja að vinna á collision

--- humans.py
+++ humans.py
@@ -1,10 +1,12 @@
 import pygame
 pygame.init()
-class student():# 7
-    def __init__(self):
+class student():#generic social humanoid
+    def __init__(self, X ,Y):
+        self.image = pygame.surface([3, 3])
+        self.image.fill(0,0,0)
+        self.rect = self.image.get_rect()
         self.connections = []
+        self.coords = [X, Y]
     def connect(self, target, score):
         self.connections.append((target, score))
         target.connections.append((self, score))
-        if target == y:
-            print("yee boi")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import humans
-x = humans.student()
-y = humans.student()
+x = humans.student(200, 200)
+y = humans.student(200, 200)
 x.connect(y, 200)
 print(x.connections)
 print(y.connections)
