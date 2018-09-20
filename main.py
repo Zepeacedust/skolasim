183deb9b3bec2090317b040b11d289235639a0bf
Zepeacedust <41028225+Zepeacedust@users.noreply.github.com>
2018-09-20 21:16:56 +0000

    náði að fá pygame til að gera screen

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import pygame, humans
+import pygame, humans, sys
 pygame.init()
 x = humans.student(200, 200)
 y = humans.student(200, 200)
 x.connect(y, 200)
+school = pygame.display.set_mode((400, 400))
+school.fill((255,255,255))
+
 print(x.connections)
 print(y.connections)
+while 1:#gameplay loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+    school.blit(x.image, x.rect)
+    school.blit(y.image, y.rect)
+    pygame.display.flip()
