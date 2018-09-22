0a01118158e6430576004e34a5825522d4ede71d
Zepeacedust <41028225+Zepeacedust@users.noreply.github.com>
2018-09-22 20:01:29 +0000

    fékk test spite til að virka

--- humans.py
+++ humans.py
@@ -4,9 +4,8 @@ pygame.init()
 class student(pygame.sprite.Sprite):#generic social humanoid
     def __init__(self, X, Y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.display.set_mode([5, 5])
-        self.image.fill((0, 0, 0))
-        self.rect = self.image.get_rect()
+        self.image = pygame.image.load("./sprites/test.JPEG")
+        self.rect = self.image.get_rect(x = X,y = Y)
         self.connections = []
         self.coords = [X, Y]
     def connect(self, target, score):
