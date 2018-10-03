6e5b707599a020189bdc26f9901606ae3ae75076
Zepeacedust <41028225+Zepeacedust@users.noreply.github.com>
2018-10-03 18:44:40 +0000

    fékk teacking til að actually virka

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-from menu import valmynd
-import humans
-
-valmynd()
-
-x = humans.student()
-y = humans.student()
-=======
 import pygame, humans, sys
 pygame.init()
 x = humans.student(200, 200, 7)
-y = humans.student(200, 600, 7)
->>>>>>> social-attributes
+y = humans.student(000, 600, 7)
+z = humans.student(600, 000, 7)
 x.connect(y, 200)
 school = pygame.display.set_mode((800, 800))
 school.fill((255,255,255))
@@ -21,11 +12,12 @@ print(y.connections)
 while 1:#gameplay loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    school.fill((255, 255, 255))
     school.blit(x.image, x.rect)
     school.blit(y.image, y.rect)
+    school.blit(z.image, z.rect)
     pygame.display.flip()
     print(x.moveto(y))
+    z.move(z.moveto(x))
     x.move(x.moveto(y))
-    y.move((+1, 0))
+    y.move((1, 0))
     pygame.time.Clock().tick(60)
