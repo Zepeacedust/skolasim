45a1c47f5695a50f102bb5370bda692629195343
Zepeacedust <41028225+Zepeacedust@users.noreply.github.com>
2018-10-16 20:52:49 +0000

    collision actually virkar

--- humans.py
+++ humans.py
@@ -4,16 +4,15 @@ pygame.init()
 class student(pygame.sprite.Sprite):#generic social humanoid
     def __init__(self, X, Y, Personality_type, student_number):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("./sprites/test.JPEG")
+        self.image = pygame.Surface((5,5))
+        self.image.fill((0,0,0))
         self.rect = self.image.get_rect(x = X,y = Y)#get_rect tekur bara x og y sem keywords
         self.pers = Personality_type
         self.connections = {}
         self.x_undermove = 0
         self.y_undermove = 0
-        self.move_counter = 300
         self.move_angle = 0, 0
         self.stud_num = student_number
-        self.scan_delay = 300
     def connect(self, target, score):
         self.connections[target] = 1 / (1 + abs(self.pers - target.pers)) * score#lætur fólk mep líkan persónuleika verða vinir hraðar
         target.connections[self] = 1 / (1 + abs(self.pers - target.pers)) * score
@@ -23,24 +22,17 @@ class student(pygame.sprite.Sprite):#generic social humanoid
         fast = (speed[0] + self.x_undermove, speed[1] + self.y_undermove)
         self.rect = self.rect.move(fast)
     def moveto(self, target):
-        if self.move_counter >= 10:
-            self.move_counter = 0
-            x_dif = self.rect.x - target.rect.x
-            y_dif = self.rect.y - target.rect.y
-            if x_dif == 0 and y_dif == 0:
-                self.move_angle = 0,0
-            else:
-                self.move_angle = (-x_dif / abs(max(x_dif, y_dif, key = abs)), -y_dif / abs(max(x_dif, y_dif, key = abs)))
-        self.move_counter += 1
-        return self.move_angle 
+        x_dif = self.rect.x - target.rect.x
+        y_dif = self.rect.y - target.rect.y
+        if x_dif == 0 and y_dif == 0:
+            self.move_angle = 0,0
+        else:
+            self.move_angle = (-x_dif / abs(max(x_dif, y_dif, key = abs)), -y_dif / abs(max(x_dif, y_dif, key = abs)))
+        return (self.move_angle)
     def scan_surroundings(self, students):
-        if self.scan_delay >= 10:
-            self.scan_delay = 0
             for stud in students:
-                if abs(stud.rect.x - self.rect.x) < 200 and abs(stud.rect.y - self.rect.y) < 200:
+                if abs(stud.rect.x - self.rect.x) < 100 and abs(stud.rect.y - self.rect.y) < 100:
                     self.connect(stud, 100 / ((abs(stud.rect.x-self.rect.x)+abs(stud.rect.y - self.rect.y))+1))
-                self.scan_delay += 1
-    
     def find_friend(self):
         top_connect = 0
         friend = self
@@ -51,14 +43,16 @@ class student(pygame.sprite.Sprite):#generic social humanoid
         return friend
     def check_collide(self, students):
         for stud in students:
-            if self.rect.colliderect(stud.rect):
-                if abs(self.rect.x - stud.rect.x) < abs(self.rect.y - stud.rect.y):
-                    self.move_angle[1] = 0
-                    stud.move_angle[1] = 0
-                elif abs(self.rect.x - stud.rect.x) > abs(self.rect.y - stud.rect.y):
-                    self.move_angle[0] = 0
-                    stud.move_angle[0] = 0
-
-
-        
-     
+            if self.rect.colliderect(stud):
+                if self.rect.bottom > stud.rect.top and stud.rect.top > self.rect.top:
+                    self.rect.bottom = stud.rect.top
+                    print("collision1")
+                if self.rect.top < stud.rect.bottom and stud.rect.bottom < self.rect.bottom:
+                    self.rect.top = stud.rect.bottom
+                    print("collision2")
+                if self.rect.left < stud.rect.right and stud.rect.right < self.rect.right:
+                    self.rect.left = stud.rect.right
+                    print("collision3")
+                if self.rect.right > stud.rect.left and stud.rect.left > self.rect.left:
+                    self.rect.right = stud.rect.left
+                    print("collision4")
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame, humans, sys, menu
 from random import randint
 
-menu.valmynd()
+scan_delay = 300
+coll_delay = 300
+move_delay = 300
+
+#menu.valmynd()
 skra = open("settings/exit.txt", "r")
 for line in skra:
     if line == "True":
@@ -17,8 +21,6 @@ student_list = []
 for stud_num in range(200):
     student_list.append(humans.student(randint(0,800), randint(0,800), randint(1,10), stud_num))
 
-# student_list[1].image = pygame.image.load("./sprites/import.JPEG")
-
 school = pygame.display.set_mode((800, 800))
 school.fill((255,255,255))
 while 1:#gameplay loop
@@ -28,8 +30,24 @@ while 1:#gameplay loop
     school.fill((255, 255, 255))
     for student in student_list:
         school.blit(student.image,student.rect)
-        student.scan_surroundings(student_list)
-        student.move(student.moveto(student.find_friend()))
+        if scan_delay >= 10:
+            student.scan_surroundings(student_list)
+        if move_delay >= 10:
+            student.move(student.moveto(student.find_friend()))
+        else:
+            student.move(student.move_angle)
         student.check_collide(student_list)
+    if scan_delay >= 10:
+        scan_delay = 0
+    else:
+        scan_delay += 1
+    if move_delay >= 10:
+        move_delay = 0
+    else:
+        move_delay += 1
+    if coll_delay >= 1:
+        coll_delay = 0
+    else:
+        coll_delay += 1
     pygame.display.flip()
     pygame.time.Clock().tick(fps)
